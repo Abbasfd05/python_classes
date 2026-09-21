62bd5d55d15bb591a6ddafad39f3cc4b7f15ecc1
Abbasfd05 <abbasfadhel2005abfd@gmail.com>
2026-09-21 21:02:52 +0300

    comments to help you understand what we have created in classes

--- classes.py
+++ classes.py
@@ -1,3 +1,7 @@
+#Python	Runs when	JavaScript equivalent
+#__init__	You create an object, like Dog('Ruby', 4)	constructor
+#__str__	You print() the object or call str() on it	toString()
+#self	Always the first parameter; the object itself	this
 class Dog():
     def __init__(self, name, age=0):
         self.name = name
